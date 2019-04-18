acb2bc24ba4a846b7e5c4ef584a5c0787822d2a3
Mike De Groot <mdegroot09@gmail.com>
2019-04-18 16:48:18 -0600

    completed project

---
+++ db/read_product.sql
@@ -0,0 +1,3 @@
+select *
+from product
+where product_id = $1
---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const massive = require('massive')
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+const ProductCtrl = require('./products_controller')
+
+app.use(express.json())
+
+massive(CONNECTION_STRING).then(dbInstance => {
+  app.set('db', dbInstance)
+  app.listen(SERVER_PORT, () => {
+    console.log('Listening to port:', SERVER_PORT)
+  })
+}).catch(err => {
+  console.log('err:', err)
+})
+
+app.get('/api/products', ProductCtrl.getAll);
+app.get('/api/products/:id', ProductCtrl.getOne);
+app.put('/api/products/:id', ProductCtrl.update)
+app.post('/api/products', ProductCtrl.create);
+app.delete('/api/products/:id', ProductCtrl.delete);
